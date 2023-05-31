6bbe11b2e3e6d56c8210c1ca1539d17e2c453483
Taxis8 <111230411+Taxis8@users.noreply.github.com>
2023-05-31 18:57:42 +0300

    v1.1

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+author 'Taxis'
+description 'Cargo Delivery By Taxis'
+version '1.1'
+
+
+client_scripts {
+	'client/main.lua',
+	'config.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua',
+}
