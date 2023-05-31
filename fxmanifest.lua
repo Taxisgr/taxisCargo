8db843dc65c5017633f54473ad01ec11bbd10364
Taxis8 <111230411+Taxis8@users.noreply.github.com>
2023-05-31 20:11:43 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+author 'Taxis'
+description 'Cargo Delivery By Taxis'
+version '1.1'
+fx_version 'cerulean'
+games {'gta5'}
+
+
+client_scripts {
+	'client/main.lua',
+	'config.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua',
+}
